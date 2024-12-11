ab3d8e0f60bb658ea6582acfed851fac912d91aa
BenjaminFaucher <bfaucher124@gmail.com>
2024-12-11 12:26:03 +0100

    Creation Procfile, requirements.txt

--- Note_de_frais.py
+++ Note_de_frais.py
@@ -204,5 +204,5 @@ def create_pdf_final(note_de_frais,ordre_de_mission,files_justificatifs):
     return(output_path)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
 
